bf01f2f6c8550ec61c38f3a0384d6f08b5e40b0b
Alex Lemm <alex23lemm@gmail.com>
2014-05-08 23:44:34 +0200

    Remove unnecessary lubridate calls; add histogram plotting

--- plot1.R
+++ plot1.R
@@ -1,14 +1,8 @@
-# Load libraries ---------------------------------------------------------------
-
-library(lubridate)
-
-
 
 # Load data --------------------------------------------------------------------
 
 hpc_df <- read.table('./raw_data/household_power_consumption.txt', 
                      header = TRUE, sep = ';', na.strings = c('?'))
-
 
 
 # Pre-process data -------------------------------------------------------------
@@ -18,17 +12,16 @@ names(hpc_df) <- tolower(names(hpc_df))
 # Subset data according to instructions
 hpc_df <- hpc_df[hpc_df$date %in% c('1/2/2007', '2/2/2007'), ]
 
-# Merge date column and time column and convert result into POSIXct format
-hpc_df$date_time <- paste(as.character(hpc_df$date), as.character(hpc_df$time),
-                          sep = "_")
-hpc_df$date_time <- dmy_hms(hpc_df$date_time)
 
-# Drop unnecessary columns
-hpc_df$date <- NULL
-hpc_df$time <- NULL
+# Create and save plot ---------------------------------------------------------
 
-
-
+png('plot1.png')
+hist(hpc_df$global_active_power, 
+     col = 'red', 
+     xlab = 'Global Active Power (kilowatts)',
+     ylab = 'Frequency', 
+     main = 'Global Active Power')
+dev.off()
 
 
 
