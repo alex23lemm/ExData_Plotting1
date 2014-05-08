e0a8f61e25cdc82d3403dd55da46ddd64e1464db
Alex Lemm <alex23lemm@gmail.com>
2014-05-08 23:26:48 +0200

    Add subsetting step

--- plot1.R
+++ plot1.R
@@ -15,6 +15,9 @@ hpc_df <- read.table('./raw_data/household_power_consumption.txt',
 
 names(hpc_df) <- tolower(names(hpc_df))
 
+# Subset data according to instructions
+hpc_df <- hpc_df[hpc_df$date %in% c('1/2/2007', '2/2/2007'), ]
+
 # Merge date column and time column and convert result into POSIXct format
 hpc_df$date_time <- paste(as.character(hpc_df$date), as.character(hpc_df$time),
                           sep = "_")
@@ -23,6 +26,8 @@ hpc_df$date_time <- dmy_hms(hpc_df$date_time)
 # Drop unnecessary columns
 hpc_df$date <- NULL
 hpc_df$time <- NULL
+
+
 
 
 
